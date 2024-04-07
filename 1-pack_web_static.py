92e1ccd1c16856a6dd27f41c0e77fe23a35fb889
Amanda Mabunda <amandamabu14@gmail.com>
2024-04-07 04:13:01 -0700

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,7 +5,7 @@
 from fabric.api import local
 from datetime import datetime
 import os.path
-
+import stat
 
 def do_pack():
     """ generates a .tgz archive from web_static"""
@@ -19,10 +19,15 @@ def do_pack():
         date_time.hour,
         date_time.minute,
         date_time.second)
-
+    print("Packing web_static to {}".format(file))
     if os.path.isdir("version") is False:
         if local('mkdir -p version').failed is True:
             return None
+
     if local('tar -cvzf {} web_static'.format(file)).failed is True:
         return None
+    os.chmod(file, stat.S_IWUSR | stat.S_IRUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH)
+
+    file_size = os.path.getsize(file)
+    print("web_static packed: {} -> {}Bytes".format(file, file_size))
     return file
